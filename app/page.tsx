03622f7d7c7e5a55c92b6953d18bab4e6b529636
fernando risso <f.risso6@hotmail.com>
2023-06-14 18:54:09 -0300

    github auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 'use client'
-import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 export default function Home() {
-
-  const session = useSession()
-  console.log(session)
-  redirect('/login')
+  redirect('/login');
 
   return (
     <main className="text-white">
