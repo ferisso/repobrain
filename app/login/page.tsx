983df462ef45d5931e3993a4124843ce80714be1
fernando risso <f.risso6@hotmail.com>
2023-06-14 16:02:42 -0300

    Rotas de login e register

---
+++ app/login/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+import Logo from "@/components/Logo";
+import { useRouter } from "next/navigation";
+import { GitHub } from "react-feather";
+
+export default function Login() {
+  const router = useRouter();
+  return (
+    <div className="flex h-screen flex-col items-center justify-center p-4 bg-zinc-50 font-light">
+      <form className="max-w-sm w-full rounded-xl shadow-lg flex flex-col gap-4 px-8 py-10 bg-white items-center">
+        <Logo height="65" className="mb-2" color="rgb(39, 39, 42)" />
+        <input type="text" placeholder="Insira o seu email" className="w-full border box-border border-zinc-300 px-4 py-2 rounded-lg focus:ring-2 focus:ring-purple-500 outline-none" />
+        <input type="password" placeholder="Insira a sua senha" className="w-full border box-border border-zinc-300 px-4 py-2 rounded-lg focus:ring-2 focus:ring-purple-500 outline-none" />
+        <button className="w-full p-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700 transition-all outlined-none text-sm">Entrar</button>
+        <p className="text-zinc-400 text-xs">ou</p>
+        <button className="w-full p-2 bg-zinc-800 hover:bg-zinc-700 transition-all text-white rounded-lg flex gap-2 justify-center items-center outlined-none text-sm">
+          <GitHub size={20} />
+          Entrar com o Github
+        </button>
+      </form>
+      <p className="mt-4 text-xs text-zinc-500">
+        Não possui uma conta?
+        <button
+          className="text-purple-600 underline ml-1 hover:text-purple-700 transition-all cursor-pointer"
+          onClick={() => router.push('/register')}
+        >
+          Cadastre-se aqui
+        </button>
+      </p>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,12 @@
-import { GitHub } from "react-feather";
-
+'use client'
+import { useRouter } from "next/navigation";
 export default function Home() {
+
+  const router = useRouter();
+  router.push('/login')
   return (
-    <main className="flex h-screen flex-col items-center justify-center p-4 bg-zinc-50">
-      <form className="max-w-sm w-full rounded-lg h-96 shadow-lg flex flex-col gap-5 p-8 bg-white items-center">
-        <h1 className="font-semibold text-lg text-zinc-700">{`{ Repobrain }`}</h1>
-        <input type="text" placeholder="Insira o seu email" className="w-full border box-border border-zinc-300 p-2 rounded-lg outline-none focus:border-purple-600" />
-        <input type="text" placeholder="Insira a sua senha" className="w-full border box-border border-zinc-300 p-2 rounded-lg outline-none focus:border-purple-600" />
-        <button className="w-full p-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700 transition-all outlined-none">Entrar</button>
-        <p className="text-zinc-400 text-xs">ou</p>
-        <button className="w-full p-2 bg-zinc-800 hover:bg-zinc-700 transition-all text-white rounded-lg flex gap-2 justify-center items-center outlined-none">
-          <GitHub size={20} />
-          Entrar com o Github
-        </button>
-      </form>
-      <p className="mt-4 text-xs text-zinc-500">
-        Não possui uma conta?
-        <a href="" className="text-purple-600 underline ml-1 hover:text-purple-700 transition-all">
-          Cadastre-se aqui
-        </a>
-      </p>
+    <main>
+
     </main>
   )
 }
