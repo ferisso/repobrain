29bc142492d64d83292eae27e36c251b0773866f
fernando risso <f.risso6@hotmail.com>
2023-06-15 15:10:46 -0300

    mostrando dados da integração com o hithub

--- app/boards/page.tsx
+++ app/boards/page.tsx
@@ -4,15 +4,21 @@ import { signOut, useSession } from "next-auth/react"
 
 export default function Boards() {
   const { data } = useSession({
-    required: true
+    required: false
   })
-  return (
-    <div>
-      boards route
-      {JSON.stringify(data)}
-      <button className="p-2 rounded-lg bg-teal-600 text-white" onClick={() => signOut()}>
-        Logout
-      </button>
-    </div>
-  )
+  if (data)
+    return (
+      <div className="flex justify-center w-full gap-8 items-center mt-2">
+        <div className="rounded-full h-10 w-10 bg-slate-600">
+          <img src={data?.user?.email || ''} alt="Clients image" />
+        </div>
+        <h2 className="text-teal-600 font-semi ">
+          Seja bem-vindo, {data?.user?.name}
+        </h2>
+        <button className="p-2 rounded-lg bg-teal-600 text-white" onClick={() => signOut()}>
+          Logout
+        </button>
+      </div>
+    )
+  return <div></div>
 }
