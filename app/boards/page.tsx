a30be3625d2eb7114209e61ddb13f6496e0d6003
fernando risso <f.risso6@hotmail.com>
2023-06-15 15:12:24 -0300

    mostrando dados da integração com o hithub

--- app/boards/page.tsx
+++ app/boards/page.tsx
@@ -7,18 +7,19 @@ export default function Boards() {
     required: false
   })
   if (data)
-    return (
-      <div className="flex justify-center w-full gap-8 items-center mt-2">
-        <div className="rounded-full h-10 w-10 bg-slate-600">
-          <img src={data?.user?.email || ''} alt="Clients image" />
-        </div>
-        <h2 className="text-teal-600 font-semi ">
-          Seja bem-vindo, {data?.user?.name}
-        </h2>
-        <button className="p-2 rounded-lg bg-teal-600 text-white" onClick={() => signOut()}>
-          Logout
-        </button>
+    console.log(data)
+  return (
+    <div className="flex justify-center w-full gap-8 items-center mt-2">
+      <div className="rounded-full h-10 w-10 bg-slate-600">
+        <img src={data?.user?.email || ''} alt="Clients image" className="h-full" />
       </div>
-    )
+      <h2 className="text-teal-600 font-semi ">
+        Seja bem-vindo, {data?.user?.name}
+      </h2>
+      <button className="p-2 rounded-lg bg-teal-600 text-white" onClick={() => signOut()}>
+        Logout
+      </button>
+    </div>
+  )
   return <div></div>
 }
