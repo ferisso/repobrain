3b82e5a499dd8aa006b1884335ebf969d20665ff
fernando risso <f.risso6@hotmail.com>
2023-06-15 15:15:35 -0300

    fix image

--- app/boards/page.tsx
+++ app/boards/page.tsx
@@ -11,7 +11,7 @@ export default function Boards() {
   return (
     <div className="flex justify-center w-full gap-8 items-center mt-2 h-full">
       <div className="rounded-full h-16 w-16 bg-slate-600">
-        <img src={data?.user?.image || ''} alt="Clients image" className="h-full" />
+        <img src={data?.user?.image || ''} alt="Clients image" className="h-full rounded-full" />
       </div>
       <h2 className="text-teal-600 font-semi text-2xl">
         Seja bem-vindo, {data?.user?.name}
