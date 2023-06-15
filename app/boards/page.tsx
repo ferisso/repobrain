99f2df549dac259c6b2cf556d6b6c97654c29ef4
fernando risso <f.risso6@hotmail.com>
2023-06-15 15:14:39 -0300

    fix image

--- app/boards/page.tsx
+++ app/boards/page.tsx
@@ -9,11 +9,11 @@ export default function Boards() {
   if (data)
     console.log(data)
   return (
-    <div className="flex justify-center w-full gap-8 items-center mt-2">
-      <div className="rounded-full h-10 w-10 bg-slate-600">
+    <div className="flex justify-center w-full gap-8 items-center mt-2 h-full">
+      <div className="rounded-full h-16 w-16 bg-slate-600">
         <img src={data?.user?.image || ''} alt="Clients image" className="h-full" />
       </div>
-      <h2 className="text-teal-600 font-semi ">
+      <h2 className="text-teal-600 font-semi text-2xl">
         Seja bem-vindo, {data?.user?.name}
       </h2>
       <button className="p-2 rounded-lg bg-teal-600 text-white" onClick={() => signOut()}>
