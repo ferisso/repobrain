e0a8620864172d3c377a741f0d54e4a5d8a7b8bf
fernando risso <f.risso6@hotmail.com>
2023-06-15 01:41:04 -0300

    redirects board

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 'use client'
+import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 export default function Home() {
+
+  const { data } = useSession()
+
+  if (data) {
+    redirect('/boards');
+  }
+
   redirect('/login');
 
   return (
